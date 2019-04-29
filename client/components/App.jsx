0d6abd0efbb9b307f75a328420931ac17ac01e14
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-04-29 07:55:54 -0700

    First commit

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
